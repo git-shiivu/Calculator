de55eeb1692f5fee87936f4cd505b099f41d17ef
Shivam Kumar <shivamku9107@gmail.com>
2025-07-21 06:36:26 +0530

    Under-Developing Phase

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const values = document.querySelector("#values");
 
-const typePad = document.querySelectorAll(".buttons");
+const typePad = document.querySelectorAll(".numbers");
+const operations = document.querySelectorAll("operation")
 
-let input = "";
+const equalTo = document.querySelector("#equalTo").innerText
+let add = false, subtract = false, multiply = false, devision = false;
+
+let input = 0;
 
 typePad.forEach(button => {
     button.addEventListener('click', () => {
-    values.innerText = input += button.innerText;    
-    });
+    input += parseInt(button.innerText);
+    values.innerText = button.innerText
+
+})
+
+});
+
+operations.forEach(button => {
+    button.addEventListener('click', () => {
+    
+        if(button.innerText == "+"){
+            console.log(button.innerText);
+            
+            add = true
+            values.innerText += button.innerText;
+        }
+        if(button.innerText == "-"){
+            subtract = true
+            values.innerText += button.innerText;
+        }
+        if(button.innerText == "x"){
+            multiply = true
+            values.innerText += button.innerText;
+        }
+        if(button.innerText == "/"){
+            devision = true
+            values.innerText += button.innerText;
+        }
+
+})
+
 });
 
 
